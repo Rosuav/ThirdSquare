e933044a124a35a3430f61f4079097933fd067e3
Chris Angelico <rosuav@gmail.com>
2015-06-18 17:08:06 +1000

    Drop the unused table

--- tickets.sql
+++ tickets.sql
@@ -6,12 +6,8 @@
 -- For convenience, it is possible to have a single database storing accounts
 -- and one set of tickets. Consequently, table names must not conflict.
 
-create table active_cards (id integer primary key, -- Must first exist in accounts::cards
-	contact varchar not null default '' -- As with account contacts, this could be email address, phone, whatever. Not managed by ThirdSquare.
-);
-
 create table tickets (id serial primary key,
-	card integer not null references active_cards,
+	card integer not null, -- references accounts::cards but not enforced in the database
 	validity smallint not null, -- Zone or whatever else defines where this is valid
 	created timestamp with time zone not null default now(), -- Mainly for statistical purposes
 	expiration timestamp with time zone not null
