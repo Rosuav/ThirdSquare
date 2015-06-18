d9bc730bd108b5abb0e6d7eb8acbd00af1c631d2
Chris Angelico <rosuav@gmail.com>
2015-06-18 12:39:01 +1000

    Distinguish table names for POC convenience

--- tickets.sql
+++ tickets.sql
@@ -3,12 +3,15 @@
 -- Each one must have a single point of contact with the accounts database,
 -- which will be recorded in its HBA file.
 
-create table cards (id integer primary key, -- Must first exist in accounts::cards
+-- For convenience, it is possible to have a single database storing accounts
+-- and one set of tickets. Consequently, table names must not conflict.
+
+create table active_cards (id integer primary key, -- Must first exist in accounts::cards
 	contact varchar not null default '' -- As with account contacts, this could be email address, phone, whatever. Not managed by ThirdSquare.
 );
 
 create table tickets (id serial primary key,
-	card integer not null references cards,
+	card integer not null references active_cards,
 	validity smallint not null, -- Zone or whatever else defines where this is valid
 	created timestamp with time zone not null default now(), -- Mainly for statistical purposes
 	expiration timestamp with time zone not null
